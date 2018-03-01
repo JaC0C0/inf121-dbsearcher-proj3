7be7dde3adacfc0ea8ce262c6ac20acdbc76c19e
VinCMG <vincentchang9999@gmail.com>
2018-02-28 23:11:40 -0800

    some cleanups in create_index

--- indexer.py
+++ indexer.py
@@ -22,72 +22,72 @@ def namedtuple_with_defaults(typename, field_names, default_values=()):
     return T
 
 class indexer():
-	def __init__(self, rootDir):
-		self.rootDir = rootDir
-		print("Indexer Created")
-		nltk.download('punkt')
-		self.Posting = namedtuple_with_defaults('Posting', 'doc_id, term_freq, tf_idf, url', ["", 0, 0, ""])
-		self.LS = LancasterStemmer()
-		self.index = defaultdict(list)
-		self.book_keeping = json.load(open(self.rootDir + '/bookkeeping.json'))
+    def __init__(self, rootDir):
+        self.rootDir = rootDir
+        print("Indexer Created")
+        nltk.download('punkt')
+        self.Posting = namedtuple_with_defaults('Posting', 'doc_id, term_freq, tf_idf, url', ["", 0, 0, ""])
+        self.LS = LancasterStemmer()
+        self.index = defaultdict(list)
+        self.book_keeping = json.load(open(self.rootDir + '/bookkeeping.json'))
 
-	def get_posting(self, stem_token):
-		return (self.index[stem_token])
+    def get_posting(self, stem_token):
+        return (self.index[stem_token])
 
-	def calculate_tf_idf(self, term_freq, num_doc_term, total_tokens, total_docs):
-		tf = term_freq / total_tokens
-		idf = log10(total_docs/num_doc_term)
-		return(tf*idf)
+    def calculate_tf_idf(self, term_freq, num_doc_term, total_tokens, total_docs):
+        tf = term_freq / total_tokens
+        idf = log10(total_docs/num_doc_term)
+        return(tf*idf)
 
-	#TODO remove. Function is for debugging
-	def record_dict(self, ddict):
-		file = open("index_dict.txt", "w")
-		for key in ddict:
-			file.write("{}:\n".format(key))
-		file.close()
+    #TODO remove. Function is for debugging
+    def record_dict(self, ddict):
+        infile = open("index_dict.txt", "w")
+        for key in ddict:
+            infile.write("{}:\n".format(key))
+        infile.close()
 
-	def create_index(self):
-    	#for debugging
-		count = 0	#TODO remove
-		#looping through all files using the bookkeeping index json
-		for file_coord in self.book_keeping:
-			count += 1	#TODO remove
-			index_pair = file_coord.split("/")
-			file = open((self.rootDir + "/{}/{}").format(index_pair[0], index_pair[1]))
-			tree = BeautifulSoup(file, 'html.parser').prettify()
-			raw = BeautifulSoup(tree, 'html.parser').get_text()
-			tokens = word_tokenize(raw)
-			for token in tokens:
-				#stemming all tokens
-				stem_token = self.LS.stem(token)
-				if not stem_token:
-					break
-				#checks to see if token has other/duplicate postings
-				if stem_token in self.index.keys():
-					#Flag to check if duplicate has been found
-					duplicate = False
-					for old_post in self.index[stem_token]:
-						#if duplicate posting, create new posting with incnremented term_freq
-						if old_post.doc_id == "doc{}{}".format(index_pair[0], index_pair[1]):
-							up_term_freq = old_post.term_freq + 1
-							new_post = self.Posting(
-								old_post.doc_id, up_term_freq, self.calculate_tf_idf(
-									up_term_freq, len(self.index[stem_token]), len(tokens), len(self.book_keeping)), old_post.url)
-							self.index[stem_token].remove(old_post)
-							duplicate = True
-							break
-					#no duplicate fouond, therefore create new posting and reset duplicate flag
-					if (not duplicate):
-						new_post = self.Posting("doc{}{}".format(index_pair[0], index_pair[1]), 1, 0, self.book_keeping[file_coord])
-						duplicate = False
-				else:
-					new_post = self.Posting("doc{}{}".format(index_pair[0], index_pair[1]), 1, 0, self.book_keeping[file_coord])
-				self.index[stem_token].append(new_post)
-				self.index[stem_token].sort(key=lambda x:x.tf_idf, reverse=True)			
+    def create_index(self):
+        doc_id_string = "{}.{}"
+        #for debugging
+        count = 0    #TODO remove
+        #looping through all infiles using the bookkeeping index json
+        for infile_coord in self.book_keeping:
+            count += 1    #TODO remove
+            index_pair = infile_coord.split("/")
+            with open((self.rootDir + "/{}/{}").format(index_pair[0], index_pair[1])) as infile:
+                tree = BeautifulSoup(infile, 'html.parser').prettify()
+                raw = BeautifulSoup(tree, 'html.parser').get_text()
+                tokens = word_tokenize(raw)
+                for token in tokens:
+                    #stemming all tokens
+                    stem_token = self.LS.stem(token)
+                    if not stem_token:
+                        break
+                    #checks to see if token has other/duplicate postings
+                    if stem_token in self.index.keys():
+                        #Flag to check if duplicate has been found
+                        duplicate = False
+                        for old_post in self.index[stem_token]:
+                            #if duplicate posting, create new posting with incnremented term_freq
+                            if old_post.doc_id == doc_id_string.format(index_pair[0], index_pair[1]):
+                                up_term_freq = old_post.term_freq + 1
+                                tf_idf = self.calculate_tf_idf(up_term_freq, len(self.index[stem_token]), len(tokens), len(self.book_keeping))
+                                new_post = self.Posting(old_post.doc_id, up_term_freq, tf_idf, old_post.url)
+                                self.index[stem_token].remove(old_post)
+                                duplicate = True
+                                break
+                        #no duplicate fouond, therefore create new posting and reset duplicate flag
+                        if (not duplicate):
+                            new_post = self.Posting(doc_id_string.format(index_pair[0], index_pair[1]), 1, 0, self.book_keeping[infile_coord])
+                            duplicate = False
+                    else:
+                        new_post = self.Posting(doc_id_string.format(index_pair[0], index_pair[1]), 1, 0, self.book_keeping[infile_coord])
+                    self.index[stem_token].append(new_post)
+                    self.index[stem_token].sort(key=lambda x:x.tf_idf, reverse=True)            
 
-			if count == 10:	#TODO remove
-				break
-		self.record_dict(self.index)	#TODO remove
+            #if count == 20:    #TODO remove
+            #    break
+        #self.record_dict(self.index)    #TODO remove
 
-	
+    
 
