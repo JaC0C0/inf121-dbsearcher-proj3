461a50b42abf9257f6bda5dcb2b475db568849fa
Dennis Tian <JaCoCo@localhost.localdomain>
2018-03-11 18:59:41 -0700

    added database prompts for creating index and allowing searching through json file

--- searcher.py
+++ searcher.py
@@ -3,22 +3,27 @@ from nltk.stem.lancaster import LancasterStemmer
 from nltk import word_tokenize
 from functools import reduce
 import string
+import json
 
 def get_intersect(post_array):
     post_array.sort(key=lambda x:len(x), reverse=False)
     new_array = []
     for posts in post_array[1:]:
         for post in posts:
-            if any(x.doc_id == post.doc_id for x in post_array[0]):
+            if any(x[0] == post[0] for x in post_array[0]):
                 new_array.append(post)
     return new_array
 
 
 def search():
-    rootDir = 'WEBPAGES/WEBPAGES_RAW'
-    the_indexer = indexer.indexer(rootDir)
-    the_indexer.create_index()
-
+    search_bool = input("Would you like to compile the search indexer? (\'y\'/\'n\')")
+    if (search_bool == "y"):
+        rootDir = 'WEBPAGES/WEBPAGES_RAW'
+        the_indexer = indexer.indexer(rootDir)
+        the_indexer.create_index()
+    infile = open("dictionary_file.txt", "r")
+    the_indexer = json.load(infile)
+    print(len(the_indexer))
     special_char_table = str.maketrans(string.punctuation, " " * len(string.punctuation))
 
     while (True):
@@ -30,21 +35,21 @@ def search():
             for token in tokens:
                 for split_token in token.translate(special_char_table).split():
                     stem_input = LS.stem(split_token)
-
-                    set_of_posts.append(the_indexer.get_posting(stem_input))
+                    set_of_posts.append(the_indexer[stem_input])
             inter_set = get_intersect(set_of_posts)
             if inter_set:
                 for posts in inter_set:
-                    print(posts.url)
+                    print(posts[-1])
             else:
                 print("No results")
         else:
             stem_input = LS.stem(tokens[0])
-            if the_indexer.get_posting(stem_input):
-                for posts in the_indexer.get_posting(stem_input):
-                    print(posts.url)
+            if the_indexer[stem_input]:
+                for posts in the_indexer[stem_input]:
+                    print(posts[-1])
             else:
                 print("No results")
+    infile.close()
 
 if __name__ == "__main__":
     search()
